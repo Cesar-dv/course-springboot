ff3058cba05eb3bd5c67f7bbab72c3f3b6229c12
Cesar-dv <cesar.acb15@gmail.com>
2021-04-08 14:03:48 -0300

    Exception handling - delete

--- src/main/java/com/trustfuldev/course/services/UserService.java
+++ src/main/java/com/trustfuldev/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.trustfuldev.course.entities.User;
 import com.trustfuldev.course.repositories.UserRepository;
+import com.trustfuldev.course.services.exceptions.DatabaseException;
 import com.trustfuldev.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete (long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
